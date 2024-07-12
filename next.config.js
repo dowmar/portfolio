1199be8271b1144d250cea4e9ad38c533daa2830
dowmar <indarya@gmail.com>
2024-07-13 03:10:57 +0700

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -1,87 +1,64 @@
 const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
-
   dest: "public",
   scope: "/",
-  sw: "service-worker.js", // Service worker file name
-  register: true, // Register service worker
+  sw: "service-worker.js",
+  register: true,
 });
 
 module.exports = withPWA({
   webpack: (config) => {
-    // Add your custom Webpack configurations here
-
-    // Return the updated config
     return config;
   },
-  //unoptimize: true, // if you want to export
   reactStrictMode: true,
   env: {
     dir: "/",
   },
   swcMinify: true,
-  /*
-Don't remove these this uses the protocol that are need to function properly
-* https://nextjs.org/docs/pages/building-your-application/optimizing/images#adding-structured-data
-
- */
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.vercel.app",
+        hostname: "*.vercel.app",
         pathname: "/api/**",
       },
       {
         protocol: "https",
-        hostname: "**.cloudinary.com",
+        hostname: "*.cloudinary.com",
         pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "**.vercel.app",
-        pathname: "",
-      },
-      {
-        protocol: "https",
-        hostname: "**.shields.io",
+        hostname: "*.shields.io",
         pathname: "/badge/**",
       },
       {
         protocol: "https",
-        hostname: "**.shields.io",
+        hostname: "*.shields.io",
         pathname: "/github/**",
       },
       {
         protocol: "https",
-        hostname: "**.githubusercontent.com",
+        hostname: "*.githubusercontent.com",
         pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "**.medium.com",
+        hostname: "*.medium.com",
         pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "**.dev.to",
+        hostname: "*.dev.to",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "github.com",
-        pathname: "",
+        pathname: "/**",
       },
     ],
   },
-
-  /*	output: {
-		// Configure your export settings here,
-		// For example, if you want to export to the "out" directory:
-		directory: 'out',
-	},
-*/
-  // https://nextjs.org/docs/messages/swc-disabled
   experimental: {
     forceSwcTransforms: true,
   },
